1c608d4ad3ad6c884f0efd3672cc7d0765594e52
junhsss <junhsssr@gmail.com>
2025-07-18 16:54:25 +0900

    fix: resolve type errors

---
+++ source/components/cookbook/template.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Task} from 'ink-task-list';
+import {TEMPLATES} from '../../utils/constants.js';
+import {Template} from '../../utils/types.js';
+import SelectInput from 'ink-select-input';
+import {useTask} from '../../hooks/usetask.js';
+import {useStep} from '../../context/stepcontext.js';
+import spinners from 'cli-spinners';
+
+export default function Template() {
+	const [state, task, , , setTask, ,] = useTask();
+	const {step, setStep, setTemplate} = useStep();
+	return (
+		<Task
+			label="Select template"
+			state={state}
+			spinner={spinners.dots}
+			isExpanded={step === 'template' && !task}
+		>
+			<SelectInput
+				items={TEMPLATES}
+				onSelect={items => {
+					setTask(items);
+					setTemplate(items as Template);
+					setStep('packagemanager');
+				}}
+			/>
+		</Task>
+	);
+}
